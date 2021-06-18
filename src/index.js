ee8269440d07cc03df127e7d0e6472f8ded974b1
irenemanas <irenemanas@gmail.com>
2021-06-18 12:25:03 +0200

    Add Counter component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.scss";
 import App from "./components/App";
+import Counter from "./components/Counter";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <HashRouter>
     <App />
+    <Counter />
   </HashRouter>,
   document.getElementById("root")
 );
