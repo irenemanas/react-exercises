2994b4ce04aefbbb0a10888cbdbf2b97cd5f10c2
irenemanas <irenemanas@gmail.com>
2021-06-18 12:16:21 +0200

    Add App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>hola mundo</p>
+      </div>
+    );
+  }
+}
+
+export default App;
